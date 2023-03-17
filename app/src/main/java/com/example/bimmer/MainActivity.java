c033fbb7ba68979df8abfcc1977ee8f0101b4686
dan1183 <klandrtp@yandex.ru>
2023-03-17 22:24:21 +0300

    Переименованы иконки, проведена инициализация компонентов в MainActivity.java

--- app/src/main/java/com/example/bimmer/MainActivity.java
+++ app/src/main/java/com/example/bimmer/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView header = (TextView) findViewById(R.id.bimmerHeader);
+        header.setText("BIMMER");
+        ImageView callBimmer = findViewById(R.id.imageCall);
+        callBimmer.setImageResource(R.drawable.icon_call);
+        ImageView profile = findViewById(R.id.iconAccount);
+        profile.setImageResource(R.drawable.icon_account);
+        Button makeAppointment = findViewById(R.id.buttonMakeAppointment);
+        makeAppointment.setText("Записаться на сервис");
     }
 
 
