627551a4aabd606253de986a499705a17123d4ca
dan1183 <klandrtp@yandex.ru>
2023-03-17 23:06:57 +0300

    Добавлен метод OnClick()

--- app/src/main/java/com/example/bimmer/MainActivity.java
+++ app/src/main/java/com/example/bimmer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bimmer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Bimmer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,70 @@ public class MainActivity extends AppCompatActivity {
         makeAppointment.setText("Записаться на сервис");
     }
 
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonMakeAppointment){
+            Log.d(TAG, "Произошло нажатие на кнопку 'Записаться на сервис'");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Context info = getApplicationContext();
+        CharSequence message = "Start activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Context info = getApplicationContext();
+        CharSequence message = "Resume activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Context info = getApplicationContext();
+        CharSequence message = "Pause activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Context info = getApplicationContext();
+        CharSequence message = "Stop activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Context info = getApplicationContext();
+        CharSequence message = "Restart activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Context info = getApplicationContext();
+        CharSequence message = "Destroy activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+    }
 
 }
